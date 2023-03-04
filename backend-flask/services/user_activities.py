8946ae7ef4a5afc4559edb03fdf5943b59e325b1
Tabish Naved <tabishnaved50@gmail.com>
2023-03-04 07:55:16 +0000

    debugged user activities

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,33 +3,34 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    with xray_recorder.begin_segment('user_activities') as segment:
-      model = {
-        'errors': None,
-        'data': None
-      }
+    
+    segment = xray_recorder.begin_segment('user_activities')
+    model = {
+      'errors': None,
+      'data': None
+    }
 
-      now = datetime.now(timezone.utc).astimezone()
+    now = datetime.now(timezone.utc).astimezone()
 
-      
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-        results = [{
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=1)).isoformat(),
-          'expires_at': (now + timedelta(days=31)).isoformat()
-        }]
-        model['data'] = results
+    
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      now = datetime.now()
+      results = [{
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=1)).isoformat(),
+        'expires_at': (now + timedelta(days=31)).isoformat()
+      }]
+      model['data'] = results
 
-      subsegment = xray_recorder.begin_subsegment('mock-data')
+    subsegment = xray_recorder.begin_subsegment('mock-data')
 
-      dict_data ={
-        "now":now.isoformat(),
-        "results-size": len(model['data'])
-      }
-      segment.put_metadata('key', dict_data, 'namespace')
-      return model
+    dict_data ={
+      "now":now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    segment.put_metadata('key', dict_data, 'namespace')
+    return model
